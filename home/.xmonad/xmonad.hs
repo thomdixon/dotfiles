99b8d8cfaf6bf59801c13e2d2e47708e1a61d427
Thom Dixon <thom@thomdixon.org>
2020-04-18 16:53:48 -0700

    Expand my Xmonad config a bit
    Now M-S-Fn will throw the current window to workspace n and switch
    to workspace n. A few programs are mapped, by default, to certain
    workspaces.

--- home/.xmonad/xmonad.hs
+++ home/.xmonad/xmonad.hs
@@ -1,10 +1,33 @@
 import XMonad
 import XMonad.Config.Desktop
+import XMonad.Util.EZConfig(additionalKeys)
+import qualified XMonad.StackSet as W
 
-myWorkspaces = ["term","web","chat","code"] ++ map show [5..9]
+-- make cmd/windows key my mod mask
+myModMask = mod4Mask
 
-main = xmonad desktopConfig 
-        { modMask = mod4Mask
-        , terminal = "terminator"
+myTerminal = "terminator"
+
+myWorkspaces = ["term", "web", "chat", "dev"] ++ map show [5..9]
+
+-- fling the most commonly used programs for those tasks to those workspaces
+myManageHook = composeAll
+    [ className =? "firefox"         --> doShiftAndGo "web"
+    , className =? "Terminator"      --> doShiftAndGo "term"
+    , className =? "TelegramDesktop" --> doShiftAndGo "chat"
+    , className =? "code-oss"        --> doShiftAndGo "dev"
+    ] where
+        doShiftAndGo ws = doF (W.greedyView ws) <+> doShift ws
+
+-- M-S-Fn flings window to workspace n and switches to it
+myKeys = [ ((shiftMask .|. myModMask, k), windows $ W.greedyView i . W.shift i)
+    | (i, k) <- zip myWorkspaces [xK_F1..xK_F9]
+    ]
+
+main = do
+    xmonad $ desktopConfig 
+        { modMask = myModMask
+        , terminal = myTerminal
         , workspaces = myWorkspaces
-        }
+        , manageHook = myManageHook <+> manageHook desktopConfig
+        } `additionalKeys` myKeys
